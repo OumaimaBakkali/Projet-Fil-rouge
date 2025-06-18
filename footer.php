352e989c9844accd85ee7a8db15d31cfb38452da
Oumaima Bakkali <bakkali.oumaima.solicode@gmail.com>
2025-06-18 15:22:48 +0100

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>StudySwap Footer</title>
+</head>
+
+<body>
+    <footer>
+        <img src="IMG/logo.png" class="footer-logo" />
+
+        <p class="footer-text">
+            StudySwap is here for you, offering a platform to exchange courses, exercises & revision notes among students.
+        </p>
+
+        <div class="social-links">
+            <img src="IMG/facebook.png">
+            <img src="IMG/instagram (1).png">
+            <img src="IMG/twitter.png">
+        </div>
+
+        <div class="footer-nav">
+            <ul class="nav-links">
+                <li><a href="#">HOME</a></li>
+                <li><a href="#">ABOUT</a></li>
+                <li><a href="#">COURSES</a></li>
+                <li><a href="#">CONTACT</a></li>
+            </ul>
+            <p class="copyright">© 2025 Studyswap, Inc. All rights reserved.</p>
+        </div>
+    </footer>
+
+</body>
+
+</html>
